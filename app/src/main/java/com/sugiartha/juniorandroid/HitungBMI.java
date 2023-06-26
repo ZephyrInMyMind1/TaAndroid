3cd7e375f274c3140f5496a02ad2b00fdc91833c
ferdianrra <ferdianrra@student.gunadarma.ac.id>
2023-06-27 06:11:29 +0700

    Mengubah pada tampilan home, menambahkan fitur & mengubah lingkaran, BMI, dan mengubah tampilan mySQL

--- app/src/main/java/com/sugiartha/juniorandroid/HitungBMI.java
+++ app/src/main/java/com/sugiartha/juniorandroid/HitungBMI.java
@@ -1,18 +1,42 @@
 package com.sugiartha.juniorandroid;
 
 public class HitungBMI {
-    public String hitungBMI(double beratBadan, double tinggiBadan){
-        String statusBadan;
-        double BMI = beratBadan/(tinggiBadan*tinggiBadan);
-        if(BMI<18.5){
-            statusBadan = "Anda kekurangan berat badan.";
-        } else if(BMI>=18.5 && BMI<=24.9){
-            statusBadan = "Berat badan anda normal (ideal).";
-        } else if(BMI>=25 && BMI<=29.9){
-            statusBadan = "Anda kelebihan berat badan.";
-        } else {
-            statusBadan = "Anda kegemukan (obesitas).";
-        }
-        return statusBadan;
+    double BMI = 0;
+    double beratIdeal;
+    double beratBadan;
+    double tinggiBadan;
+
+    public HitungBMI(double beratBadan, double tinggiBadan){
+        this.beratBadan = beratBadan;
+        this.tinggiBadan = tinggiBadan;
     }
+
+    public void setBeratIdeal(double bmi) {
+        beratIdeal = 20 * (tinggiBadan*tinggiBadan);
+    }
+
+    public double getBMI(){
+        String statusBadan;
+        BMI = beratBadan/(tinggiBadan*tinggiBadan);
+        return BMI;
+    }
+
+    public String kesimpulanBMI(double bmi){
+        String statusBadan;
+        this.setBeratIdeal(bmi);
+        if(bmi<18.5){
+            statusBadan = "Anda kekurangan berat badan.\n" +
+                    "Anda harus menaikkan berat badan sebesar " + (beratIdeal-beratBadan) + " untuk mencapai berat ideal.";
+        } else if(bmi>=18.5 && bmi<=24.9){
+            statusBadan = "Berat badan anda normal (ideal).";
+        } else if(bmi>=25 && bmi<=29.9){
+            statusBadan = "Anda kelebihan berat badan. \n" +
+                    "Anda harus menurunkan berat badan sebesar " + (beratBadan-beratIdeal) + " untuk mencapai berat ideal.";
+        } else {
+            statusBadan = "Anda kegemukan (obesitas).\n" +
+                    "Anda harus menurunkan berat badan sebesar " + (beratBadan-beratIdeal) + " untuk mencapai berat ideal.";
+        }
+        return  statusBadan;
+    }
+
 }
--- app/src/main/java/com/sugiartha/juniorandroid/HitungBMIActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/HitungBMIActivity.java
@@ -7,14 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class HitungBMIActivity extends AppCompatActivity {
-
-    HitungBMI hitungBMI = new HitungBMI(); //Membuat object dari class HitungBMI (konsep OOP biasa)
 
     EditText txtBeratBadan, txtTinggibadan; //Deklarasi object bernama txtBeratBadan dan txtTinggiBadan yang merupakan EditText
     TextView txtStatusBadan; //Deklarasi object bernama txtStatusBadan yang merupakan TextView
     Button btnCekBMI; //Deklarasi object bernama btnCekBMI yang merupakan Button
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +25,16 @@ public class HitungBMIActivity extends AppCompatActivity {
         txtTinggibadan = findViewById(R.id.txtTinggiBadan); //Menyambungkan elemen dengan id txtTinggiBadan yang ada di activity_main.xml kesini
         txtStatusBadan = findViewById(R.id.txtStatusBadan); //Menyambungkan elemen dengan id txtStatusBadan yang ada di activity_main.xml kesini
         btnCekBMI = findViewById(R.id.btnCekBMI); //Menyambungkan elemen dengan id btnCekBMI yang ada di activity_main.xml kesini
-
         //btnCekBMI.setOnClickListener dibawah ini maksudnya adalah untuk me-listen (mengamati) btnCekBMI apakah ada click, apabila ada click, maka code didalamnya akan dieksekusi
         btnCekBMI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String stringBeratBadan = txtBeratBadan.getText().toString(); //Mengambil value yang ada di EditText txtBeratBadan dan dimasukkan ke variabel stringBeratBadan
-                String stringTinggiBadan = txtTinggibadan.getText().toString(); //Mengambil value yang ada di EditText txtTinggiBadan dan dimasukkan ke variabel stringTinggiBadan
-
-                //Pada 2 baris code di atas, hasil yang didapatkan dari EditText adalah berbentuk STRING (defaultnya memang string)
-                //Sedangkan yang kita butuhkan adalah berbentuk double, maka kita convert dulu 2 string tersebut ke tipe data double
-                double beratBadan = Double.parseDouble(stringBeratBadan);
-                double tinggiBadan = Double.parseDouble(stringTinggiBadan);
-
-                String statusBadan = hitungBMI.hitungBMI(beratBadan, tinggiBadan); //Mengeksekusi method hitungBMI yang ada di class HitungBMI, karena method hitungBMI me-return string StatusBadan, maka hasil dari eksekusi method ini kita masukkan ke string statusBadan
-                txtStatusBadan.setText(statusBadan); //men-set tulisan yang ada di TextView sesuai dengan hasil dari perhitungan BMI (yaitu ada di string statusBadan)
+                double beratBadan = Double.parseDouble(txtBeratBadan.getText().toString());
+                double tinggiBadan = Double.parseDouble(txtTinggibadan.getText().toString());
+                HitungBMI hitungBMI = new HitungBMI(beratBadan, tinggiBadan);
+                double bmi = hitungBMI.getBMI();
+                String kesimpulan ="BMI Anda adalah: " + df.format(bmi) +"\n" + hitungBMI.kesimpulanBMI(bmi);
+                txtStatusBadan.setText(kesimpulan); //men-set tulisan yang ada di TextView sesuai dengan hasil dari perhitungan BMI (yaitu ada di string statusBadan)
             }
         });
     }
